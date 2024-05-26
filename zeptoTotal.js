3d63e2f1578450b8f8dde59e31e08e0bc291f69c
marco1120x <40708401+marco1120x@users.noreply.github.com>
2024-05-26 18:54:54 +0530

    Added progress bar and few adjustments

--- zeptoTotal.js
+++ zeptoTotal.js
@@ -4,19 +4,17 @@
 // @version      2024-05-21
 // @description  try to take over the world!
 // @author       You
-// @include    *://*.google*/search*
 // @include    *://*.zepto*
-// @include    *://**
 // @match        https://www.google.com/search?q=tampermonkey+firefox&client=firefox-b-d&sca_esv=cb047ac22b9af92a&ei=v59MZqm5EK7h4-EPt4mGiAY&oq=tampermonkey+&gs_lp=Egxnd3Mtd2l6LXNlcnAiDXRhbXBlcm1vbmtleSAqAggBMgsQABiABBixAxiDATIKEAAYgAQYQxiKBTIQEAAYgAQYsQMYQxiDARiKBTIKEAAYgAQYQxiKBTIKEAAYgAQYQxiKBTIKEAAYgAQYQxiKBTIKEAAYgAQYQxiKBTIFEAAYgAQyBRAAGIAEMgUQABiABEinFFDsB1jsB3ABeACQAQCYAWCgAbgBqgEBMrgBAcgBAPgBAZgCAqACX8ICChAAGLADGNYEGEfCAg0QABiABBiwAxhDGIoFmAMAiAYBkAYKkgcBMqAHlgs&sclient=gws-wiz-serp
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
 
     // Your code here...
-    console.log("script ran");
+    buttonNotFound = 0
+    console.log("script ran",buttonNotFound);
 
     //setTimeout(console.log('total',total), 3000);
     //      document.querySelectorAll('button').forEach(button => {
@@ -36,26 +34,76 @@
 
 
     // Set button text
-    button.textContent = 'Calc total';
+    button.textContent = 'Calculate Total Spending';
 
-    button.style.position = 'fixed';
-    button.style.top = '20px';
-    button.style.left = '20px';
+    button.style.position = 'sticky';
+    //     button.style.top = '64px';
+    //     button.style.left = '65px';
     button.style.zIndex = '9999';
     button.style.border = '2px';
     button.style.background = 'red';
+    button.style.padding = '3px';
+
 
     // Append the button to the body of the document
     // for now this button is not required
     //document.body.appendChild(button);
+    document.body.insertBefore(button, document.body.firstChild);
 
+    // Create the progress bar element
+    const progressBar = document.createElement('div');
+    progressBar.classList.add('progress-bar');
+    progressBar.style.height = '30px';
+    progressBar.style.backgroundColor = '#4caf50';
+    progressBar.style.width = '0%';
+    progressBar.style.borderRadius = '5px';
+
+    // Create the progress container element
+    const progressContainer = document.createElement('div');
+    progressContainer.classList.add('progress-container');
+    progressContainer.style.width = '50%';
+    progressContainer.style.backgroundColor = '#f5f5f5';
+    progressContainer.style.border = '1px solid #ccc';
+    progressContainer.style.borderRadius = '5px';
+    progressContainer.style.margin = '20px auto';
+    progressContainer.appendChild(progressBar);
+
+    progressBar.style.display = 'none';
+    progressContainer.style.display = 'none';
+
+    // Append the progress container to the body
+    document.body.appendChild(progressContainer);
+    //document.body.insertBefore(progressContainer, document.body.firstChild);
+    //const button = document.querySelector('button');
+    button.parentNode.insertBefore(progressBar, button.nextSibling);
     // Add event listener to the button
     button.addEventListener('click', function() {
         console.log('Button clicked!');
-        // Add more actions here based on button click
-    });
+        const profile = document.querySelector('a[aria-label="profile"]');
+        if (profile) {
+            profile.click();
+        } else {
+            console.log("No elements found with the aria-label 'Profile'.");
 
-    document.addEventListener('click', calc);
+        }
+        // Add more actions here based on button click
+        shouldStop = false;
+        infiniteLoopWithDelay()
+
+        progressBarFunc()
+
+
+        // timeout is added cause progressBar addition to DOM will take some time
+        setTimeout(() => {
+            const progressBar = document.querySelector('.progress-bar');
+            progressBar.style.display = 'block';
+            const progressContainer = document.querySelector('.progress-container');
+            progressContainer.style.display = 'block';
+        }, "100");
+    });
+    // document.addEventListener('click', calc);
+
+    console.log("script ran end",buttonNotFound);
 })();
 
 function calc(){
@@ -80,9 +128,120 @@ function calc(){
         });
         console.log('Total:', total);
 
-        console.log("Delayed for 3 second. ");
+        console.log("Delayed for 1 second. ");
+        const options = { style: 'decimal', useGrouping: true, notation: 'standard', compactDisplay: 'long' };
+        const formattedNumber = total.toLocaleString('en-IN', options);
+
+        // commenting alert box for now
         if(total != 0){
-            alert('Total amount spend : '+ total)
+            alert('Total amount spend : ₹'+ formattedNumber);
         }
-    }, "3000");
+        // progressBar is not required as results are out
+        resultsOut = true
+        setTimeout(() => {
+            const progressBar = document.querySelector('.progress-bar');
+            progressBar.style.display = 'none';
+            const progressContainer = document.querySelector('.progress-container');
+            progressContainer.style.display = 'none';
+        }, "10");
+
+    }, "10");
 }
+
+function loadMore(){
+    console.log("loadMore Called")
+    const button = document.querySelector('button[aria-label="Load More"]');
+
+    // Check if the button exists
+    if (button) {
+        // Programmatically click the button
+        button.click();
+    } else {
+        console.error('Button not found');
+        buttonNotFound = buttonNotFound + 1;
+    }
+    if(buttonNotFound > 3){
+        stopInfiniteLoop()
+        calc()
+    }
+
+}
+function startInterval(){
+    console.log("start interval called")
+    var intId
+    intId = setInterval(loadMore, 1000);
+    if(buttonNotFound < 3){
+        clearInterval(intId)
+    }
+    console.log("start interval end ",intId)
+}
+
+function infiniteLoopWithDelay() {
+    function loop() {
+        if (shouldStop || resultsOut) {
+            console.log('Loop has been stopped');
+            return; // Exit the loop if shouldStop is true
+        }
+        console.log('This message will print every second indefinitely');
+        loadMore()
+        setTimeout(loop, 1000);
+    }
+
+    // Start the loop
+    loop();
+}
+
+function stopInfiniteLoop() {
+    shouldStop = true;
+}
+
+function progressBarFunc(){
+
+    var shouldStopProgress = false
+    const progressBar = document.querySelector('.progress-bar');
+
+    const progressContainer = document.querySelector('.progress-container');
+
+    // Function to start the progress
+    function startProgress() {
+        console.log("start progress called")
+        let width = 0;
+
+        function frame() {
+            console.log("frame called")
+            if (width >= 100) {
+                shouldStopProgress = true
+                //clearInterval(intervalId);
+            } else {
+                width++;
+                progressBar.style.width = width + '%';
+            }
+        }
+        function infiniteLoopProgressBar() {
+            function loop1() {
+                if (shouldStopProgress || resultsOut) {
+                    console.log('Loop has been stopped');
+                    return; // Exit the loop if shouldStop is true
+                }
+                console.log('filling progress infinitely');
+                frame()
+                setTimeout(loop1, 1000);
+            }
+
+            // Start the loop
+            loop1();
+        }
+        infiniteLoopProgressBar()
+        //setInterval(frame, 100);
+    }
+    setTimeout(() => {
+        startProgress()
+    }, "10");
+
+
+}
+
+
+var shouldStop = false;
+var buttonNotFound
+var resultsOut = false;
